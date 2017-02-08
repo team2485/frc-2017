edd4b637ab574e2ab7dde1208ef5c02d8f874fc0
Nicholas Contreras <ncontreras2018@francisparker.org>
2017-02-07 18:35:35 -0800

    Added Robot side image subtraction (plus other minor things)

---
+++ src/org/usfirst/frc/team2485/commands/DriveToAndRotateTo.java
@@ -0,0 +1,30 @@
+//package org.usfirst.frc.team2485.commands;
+//
+//import org.usfirst.frc.team2485.robot.RobotMap;
+//
+//import edu.wpi.first.wpilibj.command.Command;
+//
+//public class DriveToAndRotateTo extends Command {
+//	
+//	private int inches, startAngle, endAngle, maxVelocity;
+//	private boolean dankMemes;
+//
+//	public DriveToAndRotateTo(int inches, int startAngle, int endAngle, int maxVelocity){
+//		this.inches = inches;
+//		this.startAngle = startAngle;
+//		this.endAngle = endAngle;
+//		this.maxVelocity = maxVelocity;
+//	}
+//	
+//	@Override
+//	protected void execute() {
+//		dankMemes = RobotMap.driveTrain.driveToAndRotateTo(inches, startAngle, endAngle, maxVelocity);
+//	
+//	}
+//	@Override
+//	protected boolean isFinished() {
+//	
+//		return dankMemes;
+//	}
+//
+//}
