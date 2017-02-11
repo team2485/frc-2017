4c35a28d86345a082d24a31e21493ed8ccd2b307
Nicholas Contreras <ncontreras2018@francisparker.org>
2017-02-10 18:31:20 -0800

    Added trigger drive option

--- src/org/usfirst/frc/team2485/robot/commands/DriveWithControllers.java
+++ src/org/usfirst/frc/team2485/robot/commands/DriveWithControllers.java
@@ -4,12 +4,15 @@ import org.usfirst.frc.team2485.robot.OI;
 import org.usfirst.frc.team2485.robot.RobotMap;
 import org.usfirst.frc.team2485.subsystems.DriveTrain.DriveSpeed;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveWithControllers extends Command {
 	private int mode;
 	public static int hasCheeseFlag = 0x1, useCurrentFlag = 0x2, hasSteeringCorrectionFlag = 0x4, 
 			useVelocityFlag = 0x8;
+	
+	private static final boolean TRIGGER_DRIVE = false;
 
 	public DriveWithControllers(int mode) {
 		requires(RobotMap.driveTrain);
@@ -34,6 +37,11 @@ public class DriveWithControllers extends Command {
 		} else {
 			RobotMap.driveTrain.setDriveSpeed(DriveSpeed.NORMAL_SPEED_RATING);
 		}
+		
+		if (TRIGGER_DRIVE) {
+			foward = OI.xBox.getRawAxis(OI.XBOX_RTRIGGER) - OI.xBox.getRawAxis(OI.XBOX_LTRIGGER);
+		}
+		
 		RobotMap.driveTrain.warlordDrive(foward, right, (mode & useCurrentFlag) != 0, (mode & hasCheeseFlag) != 0, 
 				(mode & hasSteeringCorrectionFlag) != 0, (mode & useVelocityFlag) != 0);
 
