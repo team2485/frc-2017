b62b2d50b27f6184801ba0a6d6130ae9cead83ea
Driver Station Computer <driver@station.com>
2017-02-14 20:52:26 -0800

    Made changes to pid things

--- src/org/usfirst/frc/team2485/util/PIDSourceWrapper.java
+++ src/org/usfirst/frc/team2485/util/PIDSourceWrapper.java
@@ -4,27 +4,25 @@ import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 
 public class PIDSourceWrapper implements PIDSource {
-	public PIDSourceWrapper(WARLordsPIDSource pidSource) {
-		// TODO Auto-generated constructor stub
-		this.pidSource = pidSource;
-	}
+	
 	private WARLordsPIDSource pidSource;
 
+	public void setPidSource(WARLordsPIDSource pidSource) {
+		this.pidSource = pidSource;
+	}
+	
 	@Override
 	public PIDSourceType getPIDSourceType() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public double pidGet() {
-		// TODO Auto-generated method stub
 		return pidSource.pidGet();
 	}
 
 	@Override
 	public void setPIDSourceType(PIDSourceType arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 	
